72b709e79908ecf4e0963a617c9fd2fc1c5b0334
MaruyamaYuuki <k023g0106@g.neec.ac.jp>
2024-09-09 15:56:09 +0900

    ボスのモデルの変更+それに伴ってボスの縦幅の変更

--- DirectXGame/Boss.cpp
+++ DirectXGame/Boss.cpp
@@ -11,7 +11,7 @@ void Boss::Initialize(Model* model, ViewProjection* viewProjection) {
 	soundDataHandle_ = audio_->LoadWave("HitSE.wav");
 	worldTransform_.translation_ = {
 	    10.0f,
-	    -20.0f,
+	    -8.5f,
 	   0.0,
 	};
 }
