03d38cc0e3e780f4fafa3d78fa864f7c6e9509cd
MaruyamaYuuki <k023g0106@g.neec.ac.jp>
2024-09-11 15:25:59 +0900

    クリアシーンにテキストの追加

--- DirectXGame/ClearScene.h
+++ DirectXGame/ClearScene.h
@@ -22,6 +22,11 @@ private:
 	Skydome* skydome_ = nullptr;
 	//Model* modelFlont_ = nullptr;
 	bool isfinish = false;
+
+	float moveSpeed_ = 0.01f; // 移動速度
+	float moveRange_ = 1.0f;  // 移動範囲
+	int moveDirection_ = 1;   // 1: 右移動, -1: 左移動
+	float initialPositionX_;  // 初期位置のX座標
 };
 
 #endif // CLEARSCENE
