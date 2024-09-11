5a38389decbcea0114d2c727959061a8ceba415f
taohdghas <143993457+taohdghas@users.noreply.github.com>
2024-09-11 14:52:04 +0900

    天球描画

--- DirectXGame/Instruction.cpp
+++ DirectXGame/Instruction.cpp
@@ -1,16 +1,24 @@
 #include "Instruction.h"
 #include "Input.h"
 #include "DirectXCommon.h"
-Instruction::~Instruction() { delete modelBackground_; }
+Instruction::~Instruction() { 
+	delete modelBackground_;
+	delete modelSkyDome_;
+	delete skydome_;
+}
 
 void Instruction::Initialize() {
 	worldTransform_.Initialize();
 	viewProjection_.Initialize();
-	modelBackground_ = Model::CreateFromOBJ("title1", true);
+	modelBackground_ = Model::CreateFromOBJ("ruleText", true);
+	modelSkyDome_ = Model::CreateFromOBJ("sphere", true);
+	skydome_ = new Skydome();
+	skydome_->Initialize(modelSkyDome_, &viewProjection_);
 	isfinish = false;
 }
 
 void Instruction::Update() {
+	skydome_->Update();
 	worldTransform_.UpdateMatrix();
 	if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
 		isfinish = true;
@@ -23,5 +31,6 @@ void Instruction::Draw() {
 
 	Model::PreDraw(commandList);
 	modelBackground_->Draw(worldTransform_, viewProjection_);
+	skydome_->Draw();
 	Model::PostDraw();
 }
